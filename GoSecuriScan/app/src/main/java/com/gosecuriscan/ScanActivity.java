4da11895588886e6873551e1d6f5f0e972e161bc
AntoineDeboeuf <antoine.deboeuf@outlook.com>
2019-02-10 19:30:40 +0100

    GoSecuriScann, v2 avec reconnaissance de caractere

--- GoSecuriScan/app/src/main/java/com/gosecuriscan/ScanActivity.java
+++ GoSecuriScan/app/src/main/java/com/gosecuriscan/ScanActivity.java
@@ -1,31 +1,28 @@
 package com.gosecuriscan;
 
-import android.Manifest;
 import android.app.Activity;
 import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
-import android.os.Environment;
+import android.os.StrictMode;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ImageView;
 
 import java.io.File;
-import java.io.IOException;
+import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
 
 public class ScanActivity extends AppCompatActivity {
 
-    private static final int MY_CAMERA_PERMISSION_CODE = 100;
-    private static final int CAMERA_REQUEST = 1888;
-    private ImageView imageView;
+    ImageView imageView;
+    Uri image;
+    String mCameraFileName;
 
-    //A la création on prend l'image
+    //A la création on prend l'imageView pour pouvoir la modifier plus tard :
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,22 +30,45 @@ public class ScanActivity extends AppCompatActivity {
         imageView = (ImageView) this.findViewById(R.id.imageView2);
     }
 
+    //Mettre la photo prise dans l'image view :
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (requestCode == CAMERA_REQUEST && resultCode == Activity.RESULT_OK) {
-            Bitmap photo = (Bitmap) data.getExtras().get("data");
-            imageView.setImageBitmap(photo);
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == Activity.RESULT_OK) {
+            if (requestCode == 2) {
+                if (data != null) {
+                    image = data.getData();
+                    imageView.setImageURI(image);
+                    imageView.setVisibility(View.VISIBLE);
+                    System.out.println("l'image est chargée dans l'image view");
+                }
+            }
         }
     }
 
-    public void camera(View view) {
-        if (checkSelfPermission(Manifest.permission.CAMERA)
-                != PackageManager.PERMISSION_GRANTED) {
-            requestPermissions(new String[]{Manifest.permission.CAMERA},
-                    MY_CAMERA_PERMISSION_CODE);
-        } else {
-            Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
-            startActivityForResult(cameraIntent, CAMERA_REQUEST);
-        }
+    //Lancer la camera pour prendre la photo :
+    public void cameraIntent(View view) {
+        StrictMode.VmPolicy.Builder builder = new StrictMode.VmPolicy.Builder();
+        StrictMode.setVmPolicy(builder.build());
+        Intent intent = new Intent();
+        intent.setAction(MediaStore.ACTION_IMAGE_CAPTURE);
+
+        Date date = new Date();
+        DateFormat df = new SimpleDateFormat("dd-MM-yyyy--kk-mm-ss");
+
+        String newPicFile = df.format(date) + ".jpg";
+        String outPath = newPicFile;
+        File outFile = new File(outPath);
+
+        mCameraFileName = outFile.toString();
+        Uri outuri = Uri.fromFile(outFile);
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, outuri);
+        startActivityForResult(intent, 2);
     }
+
+
+    /*public void camera (View v) {
+        Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(cameraIntent, CAM_REQUEST);
+    }*/
 }
